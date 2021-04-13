00fb1236e5b68f02dab628e55b9f2df537b4d051
JaspinderPawar <jaspinder@ucreate.co.in>
2021-04-13 17:46:30 +0530

    Remove dead code

--- graphql/permission/authenticated.js
+++
@@ -1,6 +0,0 @@
-const { AuthenticationError } = require('apollo-server-express')
-const { skip } = require('graphql-resolvers')
-
-const isAuthenticated = (root, args, { user }) => user ? skip : new AuthenticationError('Not authenticated');
-
-module.exports = { isAuthenticated, };
