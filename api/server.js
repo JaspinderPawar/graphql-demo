6af803a86c1318e1bcd726d45fa9871647c37a90
JaspinderPawar <jaspinder@ucreate.co.in>
2021-04-19 12:04:33 +0530

    Update server to work with asyc approach

--- api/server.js
+++ api/server.js
@@ -3,31 +3,37 @@ const { createServer } = require('http');
 const { ApolloServer, makeExecutableSchema } = require('apollo-server-express');
 import { applyMiddleware } from "graphql-middleware"
 const cors = require('cors');
+const { createSequelize } = require('../database/models');
 const typeDefs = require('../graphql/schemas');
-const resolvers = require('../graphql/resolvers');
-const context = require('../graphql/context');
+const { createResolvers } = require('../graphql/resolvers');
+const { createContext } = require('../graphql/context');
 const permissions = require('../graphql/permisssions');
 const app = express();
 
-app.use(cors());
+const createHttpServer = async () => {
+  app.use(cors());
 
-const apolloServer = new ApolloServer({
-  schema: applyMiddleware(
-    makeExecutableSchema({ typeDefs, resolvers }),
-    permissions
-  ),
-  context,
-  introspection: true,
-  playground: {
-    settings: {
-      'schema.polling.enable': false,
-      'editor.fontSize': 18,
+  const sequelize = await createSequelize();
+  const resolvers = createResolvers({ sequelize })
+  const context = createContext({ sequelize })
+  const apolloServer = new ApolloServer({
+    schema: applyMiddleware(
+      makeExecutableSchema({ typeDefs, resolvers }),
+      permissions
+    ),
+    context,
+    introspection: true,
+    playground: {
+      settings: {
+        'schema.polling.enable': false,
+        'editor.fontSize': 18,
+      },
     },
-  },
-});
+  });
 
-apolloServer.applyMiddleware({ app, path: '/api' });
+  apolloServer.applyMiddleware({ app, path: '/api' });
 
-const server = createServer(app);
-
-module.exports = server;
+  const server = createServer(app);
+  return server
+}
+module.exports = { createHttpServer };
--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 require('dotenv').config();
 
-const server = require('./api/server');
+const { createHttpServer } = require('./api/server');
 
-const port = process.env.PORT || 3301;
+(async () => {
+  const server = await createHttpServer();
+  const port = process.env.PORT || 3301;
 
-process.on('uncaughtException', (err) => {
-  console.error(`${(new Date()).toUTCString()} uncaughtException:`, err);
-  process.exit(0);
-});
-process.on('SIGINT', (err) => {
-  console.error(`${(new Date()).toUTCString()} SIGINT:`, err);
-  process.exit(0);
-});
-process.on('SIGTERM', (err) => {
-  console.error(`${(new Date()).toUTCString()} SIGTERM:`, err);
-  process.exit(0);
-});
+  process.on('uncaughtException', (err) => {
+    console.error(`${(new Date()).toUTCString()} uncaughtException:`, err);
+    process.exit(0);
+  });
+  process.on('SIGINT', (err) => {
+    console.error(`${(new Date()).toUTCString()} SIGINT:`, err);
+    process.exit(0);
+  });
+  process.on('SIGTERM', (err) => {
+    console.error(`${(new Date()).toUTCString()} SIGTERM:`, err);
+    process.exit(0);
+  });
 
-process.on('ELIFECYCLE', (err) => {
-  console.error(`${(new Date()).toUTCString()} ELIFECYCLE:`, err);
-  process.exit(0);
-});
-process.on('unhandledRejection', (err) => {
-  console.error(`${(new Date()).toUTCString()} unhandledRejection:`, err);
-});
+  process.on('ELIFECYCLE', (err) => {
+    console.error(`${(new Date()).toUTCString()} ELIFECYCLE:`, err);
+    process.exit(0);
+  });
+  process.on('unhandledRejection', (err) => {
+    console.error(`${(new Date()).toUTCString()} unhandledRejection:`, err);
+  });
 
 
-server.listen({ port }, () => console.log(
-  `🚀 Server ready at http://localhost:${port}/api`,
-));
+  server.listen({ port }, () => console.log(
+    `🚀 Server ready at http://localhost:${port}/api`,
+  ));
+})();
