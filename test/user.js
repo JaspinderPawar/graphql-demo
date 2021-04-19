e4e40764d1efc5a7254634934cf6911be166277c
JaspinderPawar <jaspinder@ucreate.co.in>
2021-04-19 12:09:26 +0530

    Update test case

--- test/user.js
+++ test/user.js
@@ -1,45 +1,44 @@
-
 const { describe, it } = require('mocha');
 const assert = require('assert');
 import { createTestClient } from 'apollo-server-integration-testing';
 
 const typeDefs = require('../graphql/schemas');
-const resolvers = require('../graphql/resolvers');
-;
+const { createSequelize } = require('../database/models');
+const { createResolvers } = require('../graphql/resolvers');
 const { ApolloServer } = require('apollo-server-express');
+const mutationHelper = require('./mutate')
 
 
 describe('Users#user', () => {
 
+    let sequelize;
+    let resolvers;
+
+    before(async () => {
+        /* eslint-disable  prefer-destructuring */
+        sequelize = await createSequelize();
+        resolvers = createResolvers({ sequelize })
+
+        /* eslint-enable  prefer-destructuring */
+    });
+
+    beforeEach(() => sequelize.truncate());
+
     it('Register and login', async () => {
         const apolloServer = new ApolloServer({ typeDefs, resolvers });
-        const { query, mutate } = createTestClient({
+        const { mutate } = createTestClient({
             apolloServer,
         });
 
-        const registerUser = `
-            mutation register ($email: String!, $name: String!, $password: String!){
-                register(input: { email: $email, password: $password, name: $name }) {
-                  name
-                  email
-                }
-              }
-            `;
+        const { registerMutate, loginMutate } = mutationHelper;
 
         const user = { email: 'nancy@foo.co', name: 'john', password: 'password' };;
-        const { data: { register } } = await mutate(registerUser, {
+        const { data: { register } } = await mutate(registerMutate, {
             variables: user,
         });
 
         assert.deepStrictEqual(register, { name: user.name, email: user.email });
 
-        const loginMutate = `
-        mutation login ($email: String!, $password: String! ){
-            login(input: { email: $email, password: $password  }) {
-                token
-            }
-          }
-        `;
         const { data: { login } } = await mutate(loginMutate, {
             variables: user,
         });
