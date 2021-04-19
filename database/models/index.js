69c8bee174be7899ef3bbef0b4aa3b639c1c50be
JaspinderPawar <jaspinder@ucreate.co.in>
2021-04-19 12:03:24 +0530

    update models to return an instance

--- database/models/index.js
+++ database/models/index.js
@@ -9,41 +9,64 @@ const env = process.env.NODE_ENV || 'development';
 
 const config = require('../config/config')[env];
 
-const db = {};
+const createSequelize = async () => {
 
-const {
-  url,
-  sync,
-  logging,
-  operatorsAliases,
-  ssl,
-  dialectOptions,
-} = config;
 
-const sequelize = new Sequelize(url, {
-  dialect: 'postgres',
-  logging,
-  operatorsAliases,
-  ssl,
-  dialectOptions,
-});
+  const db = {};
 
-fs.readdirSync(__dirname)
-  .filter((file) => (
-    file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
-  ))
-  .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
+  const {
+    url,
+    sync,
+    logging,
+    operatorsAliases,
+    ssl,
+    dialectOptions,
+  } = config;
+
+  const sequelize = new Sequelize(url, {
+    dialect: 'postgres',
+    logging,
+    operatorsAliases,
+    ssl,
+    dialectOptions,
   });
 
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+  fs.readdirSync(__dirname)
+    .filter((file) => (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    ))
+    .forEach((file) => {
+      const model = sequelize.import(path.join(__dirname, file));
+      db[model.name] = model;
+    });
+
+  Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
+    }
+  });
+
+  db.sequelize = sequelize;
+  db.Sequelize = Sequelize;
+
+  if (sync) {
+    await sequelize.sync({ force: true });
   }
-});
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+  return {
+    db,
+    models: sequelize.models,
+    close: () => sequelize.connectionManager.close(),
+    truncate: async () => {
+      await db.sequelize.models.User.truncate({ cascade: true });
+      await Promise.all(
+        Object.values(db.sequelize.models)
+          .map(model => model.truncate({ cascade: true })),
+      );
+    },
+  };
 
-module.exports = db;
+
+}
+
+module.exports = { createSequelize }
